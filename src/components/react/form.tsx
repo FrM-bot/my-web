1926d85989ee963fa0ad4b8f57717a040f1f1774
Franco M <damianmaciel0@gmail.com>
2022-09-29 20:22:45 -0300

    add more gap in intro section

--- src/components/react/form.tsx
+++ src/components/react/form.tsx
@@ -36,25 +36,23 @@ const Form = () => {
   const handlerSetStatus = () => setStatus('')
 
   return (
-        <div className='flex flex-col gap-2'>
-            <form ref={refForm} onSubmit={onSubmit} className="grid grid-cols-1 w-full shadow-lg p-4 rounded-md max-w-2xl">
-                <Input label='Your name:' name='name' props={{ placeholder: 'Jhon Rambo', type: 'text' }} />
-                <Input label='Your email:' name='email' props={{ placeholder: 'jhon_rambo@email.com', type: 'email', required: true }} />
-                <label htmlFor="message">
-                    <TextSubline>Your message:</TextSubline>
-                </label>
-                    <textarea className='dark:bg-custom-dark-2 border-[1px] border-[#b4b2b27d] dark:border-custom-dark rounded-md w-full p-1 focus:outline-hidden my-2 shadow-sm resize-none' name="message" required placeholder='Hi, my name is Jhon Rambo...' />
-                <Button props={{ type: 'submit', disabled: isLoading }}>{ isLoading ? 'Sending...' : 'Send'}</Button>
-                {
-                    status &&
-                    <div className='grid place-content-center my-2'>
-                        <Card props={{ onClick: handlerSetStatus }}>
-                            <p>{status}</p>
-                        </Card>
-                    </div>
-                }
-            </form>
+    <form ref={refForm} onSubmit={onSubmit} className="grid grid-cols-1 w-full shadow-lg p-4 rounded-md max-w-2xl">
+      <Input label='Your name:' name='name' props={{ placeholder: 'Jhon Rambo', type: 'text' }} />
+      <Input label='Your email:' name='email' props={{ placeholder: 'jhon_rambo@email.com', type: 'email', required: true }} />
+      <label htmlFor="message">
+        <TextSubline>Your message:</TextSubline>
+      </label>
+      <textarea className='dark:bg-custom-dark-2 border-[1px] border-[#b4b2b27d] dark:border-custom-dark rounded-md w-full p-1 focus:outline-hidden my-2 shadow-sm resize-none' name="message" required placeholder='Hi, my name is Jhon Rambo...' />
+      <Button props={{ type: 'submit', disabled: isLoading }}>{isLoading ? 'Sending...' : 'Send'}</Button>
+      {
+        status &&
+        <div className='grid place-content-center my-2'>
+          <Card props={{ onClick: handlerSetStatus }}>
+            <p>{status}</p>
+          </Card>
         </div>
+      }
+    </form>
   )
 }
 
